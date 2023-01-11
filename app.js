ea244f06e359b0919c4525dc33e75950e70e738e
Hong Fung <khoohf-wm21@student.tarc.edu.my>
2023-01-11 17:43:29 +0800

    initial commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 require('express-async-errors');
 
+const path = require('path');
 // extra security packages
 const helmet = require('helmet');
 const cors = require('cors');
@@ -28,17 +29,19 @@ app.use(rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per wimdowMs
 }));
+app.use(express.static(path.resolve(__dirname, './client/build')));
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
 
 // routes
-app.use('/', (req, res) => {
-  res.send('<h3>Jobs API</h3><a href="/api/v1/auth/register">register</a>')
-})
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authentication, jobsRouter)
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
