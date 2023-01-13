a30711f9a97a5eef50974fbed9f53cb2bb2a03e4
Hong Fung <khoohf-wm21@student.tarc.edu.my>
2023-01-13 10:48:15 +0800

    initial commit

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const path = require('path');
 const helmet = require('helmet');
 const cors = require('cors');
 const xss = require('xss-clean');
-const rateLimit = require('express-rate-limit')
 
 const express = require('express');
 const app = express();
@@ -25,10 +24,6 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.set('trust proxy', 1);
-app.use(rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per wimdowMs
-}));
 app.use(express.static(path.resolve(__dirname, './client/build')));
 app.use(express.json());
 app.use(helmet());
@@ -46,7 +41,7 @@ app.get('*', (req, res) => {
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const router = express.Router()
-
+const rateLimiter = require('express-rate-limit')
 const { login, register } = require('../controllers/auth')
 
-router.post('/register', register)
-router.post('/login', login)
+const apiLimiter = rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per wimdowMs
+    message: {
+        msg: 'Too many requests from this IP, please try again after 15 minutes',
+    },
+})
+
+router.post('/register', apiLimiter, register)
+router.post('/login', apiLimiter, login)
 
 module.exports = router
